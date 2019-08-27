a1db85ba59ed70d1b221244cb98b99a2f406c689
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2019-08-26 22:45:35 -0700

    Run cypress examples

---
+++ cypress/integration/github.spec.js
@@ -0,0 +1,23 @@
+context("GitHub", () => {
+  beforeEach(() => {});
+
+  it("should work", () => {
+    cy.visit("https://developer.mozilla.org/en-US/");
+    // cy.visit("https://benjamminj.github.io/testing-react-apps-talk/");
+
+    // cy.contains("View the Demo").click();
+    // cy.contains("Topic").click();
+    // cy.focused().type("Cypress");
+    // cy.contains("Search!").click();
+
+    // cy.contains("cypress");
+    // cy.contains(
+    //   "Fast, easy and reliable testing for anything that runs in a browser."
+    // )
+    //   .siblings()
+    //   .contains("See More")
+    //   .click();
+
+    // cy.contains("MIT License");
+  });
+});
